a0c3710b807f3720e798b6e0b11967caaee625b1
Samuel Adekoya <samueladexx@gmail.com>
2025-05-23 18:40:21 +0100

    fix issue with database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from "drizzle-kit";
 import "dotenv/config";
 
+const DB_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.DATABASE_URL
+    : process.env.PROD_DATABASE_URL;
+
 export default {
   schema: "./src/lib/db/schema",
   dialect: "postgresql",
   out: "./src/lib/db/migrations",
   dbCredentials: {
-    url: process.env.DATABASE_URL as string,
+    url: DB_URL as string,
   },
 } satisfies Config;
